292649d8bd675884755f6263105cfa4757096dfc
渡邉将太 <shotawebcoding@gmail.com>
2022-12-24 16:06:17 +0900

    push

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Task, Profile
 
-# Register your models here.
+# 確認したいモデルを登録する。admin.site.register(クラス名)
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Profile)
